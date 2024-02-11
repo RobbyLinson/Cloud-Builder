ac2596d9b49c42662b11a18ff806d62141294c40
EBrenne228 <127849218+EBrenne228@users.noreply.github.com>
2024-02-11 22:21:40 +0000

    Adding graphic/description to CLI
    -added ascii graphic
    -reverted chalk version to an older version as the newest has import problems
    -provided description of the greeting command yuuv implemented
    -clears console so graphic doesn't show again on calling the greet command(might need to change greet command to a yargs option instead of a command)

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 #!/usr/bin/env node
 
 // Import the yargs library
+
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
+const chalk = require('chalk');
+//const boxen = require('boxen');
+
+
+//make logo
+
+
+
+
+
+
+
+
+console.log(chalk.blueBright("  ____ _                 _   _           _ _     _           "));
+console.log(chalk.blueBright(" / ___| | ___  _   _  __| | | |__  _   _(_) | __| | ___ _ __ "));
+console.log(chalk.blueBright("| |   | |/ _ \| | | |/ _` | | '_ \| | | | | |/ _` |/ _ \ '__|"));
+console.log(chalk.blueBright("| |___| | (_) | |_| | (_| | | |_) | |_| | | | (_| |  __/ |   "));
+console.log(chalk.blueBright(" \____|_|\___/ \__,_|\__,_| |_.__/ \__,_|_|_|\__,_|\___|_|   "));
+
+
+console.log("Welcome to cloud builder");
+
+console.log("\n commands:\n greet-Cli  greet yourNameHere       Gives you a little greeting!")
 
 // Use yargs to define commands and their callbacks
 yargs(hideBin(process.argv))
   .command('greet [name]', 'greet a user by name', (yargs) => {
+    console.clear();
     return yargs.positional('name', {
       describe: 'name to greet',
       type: 'string',
