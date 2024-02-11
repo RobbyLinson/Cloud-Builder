affc34deda3df44adc0d0a89337f1df3f5ee96c1
Michał Naklicki <rhobariii567@gmail.com>
2024-02-11 21:00:27 +0000

    Changed repetitive code

--- provider.js
+++ provider.js
@@ -8,113 +8,40 @@ async function providerAws({
 	secretAccessKey
 }) {
 
-	async function createVpc({
-		name,
-		cidrBlock,
-		...options
-	}) {
-		const ec2Client = new EC2Client({
-			region,
-			credentials: {
-				accessKeyId,
-				secretAccessKey
-			}
-		});
-		const command = new CreateVpcCommand({
-			CidrBlock: cidrBlock
-			// this needs to be able to handle all of the optional parameters in "options"
-			// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ec2/command/CreateVpcCommand/
-		});
-		const response = await ec2Client.send(command);
-		return {
-			metadata: response.$metadata, // return metadata for debugging
-			vpc: response.Vpc
-		};
-	}
-
-	async function createSubnet({
-		name,
-		mainVpc,
-		cidrBlock,
-		...options
-	}) {
-		const ec2Client = new EC2Client({
-			region,
-			credentials: {
-				accessKeyId,
-				secretAccessKey
-			}
-		});
-		const command = new CreateSubnetCommand({
-			VpcId: mainVpc.id,
-			CidrBlock: cidrBlock
-			// this needs to be able to handle all of the optional parameters in "options"
-			// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ec2/command/CreateSubnetCommand/
-		});
-		const response = await ec2Client.send(command);
-		return {
-			metadata: response.$metadata, // return metadata for debugging
-			subnet: response.Subnet
-		};
-	}
-	
-	// CreateInstanceCommand was here first but it doesn't actually seem to exist
-	async function createInstance({
-		name,
-		subnet,
-		imageId,
-		...options
-	}) {
-		const ec2Client = new EC2Client({
-		region,
-		credentials: {
-			accessKeyId,
-			secretAccessKey
-		}
-		});
-		const command = new RunInstancesCommand({
-			// some required parameters are missing here + the optional parameters
-			// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ec2/command/RunInstancesCommand/
-			SubnetId: subnet.id,
-			ImageId: imageId
-		});
-		const response = await ec2Client.send(command);
-		return {
-			metadata: response.$metadata,
-			groups: response.Groups, // has the description "Not supported." in the documentation, unsure if needed
-			instances: response.Instances,
-			ownerId: response.OwnerId,
-			requesterId: response.RequesterId,
-			reservationId: response.reservationId
-		};
-	}
-
 	async function createResource({
 		type,
-		name,
 		...options
 	}) {
+		const ec2Client = new EC2Client({
+			region,
+			credentials: {
+				accessKeyId,
+				secretAccessKey
+			}
+		});
+		
+		var command;
 		switch (type) {
 		case 'vpc':
-			return createVpc({
-				name,
-				...options
-			});
+			// validate options here once this is merged with Options branch
+			command = new CreateVpcCommand(options);
+			break;
 		case 'subnet':
-			return createSubnet({
-				name,
-				...options
-			});
+			// validate options here once this is merged with Options branch
+			command = new CreateSubnetCommand(options);
+			break;
 		case 'instance':
-			return createInstance({
-				name,
-				...options
-			});
+			// validate options here once this is merged with Options branch
+			command = new RunInstancesCommand(options);
+			break;
 		default:
 			return {
 				error: `Unknown resource type: ${type}`
 			};
 		}
+		
+		const response = await ec2Client.send(command);
+		return response;
 	}
 
 	return {
