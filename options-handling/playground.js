3233aeb33f516cc0de6d4f5aafc2ca218fcc88ee
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2024-02-13 13:38:58 +0000

    bugs fixed

--- options-handling/playground.js
+++ options-handling/playground.js
@@ -1,16 +1,14 @@
-import { ACCESS_KEY, SECRET_ACCESS_KEY, REGION } from "../credentials.js" // temporary
 import { createVPC } from "./vpc-actions.js"
 
 // I didn't run the code, maybe this part should be inside createVPC. 
 // In Provider branch this is handled, so we don't have to worry about it.
-const ec2client = new EC2Client({
-    region: REGION,
-    accessKeyId: ACCESS_KEY,
-    secretAccessKey: SECRET_ACCESS_KEY 
-});
 
 const input = {
-    CidrBlock: "120.0.0.0/16",
+    CidrBlock: "12.0.0.0/16",
+    // DryRun: true,
+    Tags: [
+        {name: "TestVPC"},
+    ]
 }
 
-createVPC(input, ec2client); // I will temporarily pass the ec2client var in the function
+createVPC(input);
