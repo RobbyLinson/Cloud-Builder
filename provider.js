2084f6795daa075efbcfb407ec9c19631299635f
Michał Naklicki <rhobariii567@gmail.com>
2024-02-13 12:29:01 +0000

    Functions back in providerAws

--- provider.js
+++ provider.js
@@ -10,7 +10,6 @@ async function providerAws({
 
 	async function createVpc({
 		name,
-		cidrBlock,
 		...options
 	}) {
 		const ec2Client = new EC2Client({
@@ -20,22 +19,14 @@ async function providerAws({
 				secretAccessKey
 			}
 		});
-		const command = new CreateVpcCommand({
-			CidrBlock: cidrBlock
-			// this needs to be able to handle all of the optional parameters in "options"
-			// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ec2/command/CreateVpcCommand/
-		});
+		// validation of the options object can take place here
+		const command = new CreateVpcCommand(options);
 		const response = await ec2Client.send(command);
-		return {
-			metadata: response.$metadata, // return metadata for debugging
-			vpc: response.Vpc
-		};
+		return response;
 	}
 
 	async function createSubnet({
 		name,
-		mainVpc,
-		cidrBlock,
 		...options
 	}) {
 		const ec2Client = new EC2Client({
@@ -45,48 +36,27 @@ async function providerAws({
 				secretAccessKey
 			}
 		});
-		const command = new CreateSubnetCommand({
-			VpcId: mainVpc.id,
-			CidrBlock: cidrBlock
-			// this needs to be able to handle all of the optional parameters in "options"
-			// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ec2/command/CreateSubnetCommand/
-		});
+		// validation of the options object can take place here
+		const command = new CreateSubnetCommand(options);
 		const response = await ec2Client.send(command);
-		return {
-			metadata: response.$metadata, // return metadata for debugging
-			subnet: response.Subnet
-		};
+		return response;
 	}
 	
-	// CreateInstanceCommand was here first but it doesn't actually seem to exist
 	async function createInstance({
 		name,
-		subnet,
-		imageId,
 		...options
 	}) {
 		const ec2Client = new EC2Client({
-		region,
-		credentials: {
-			accessKeyId,
-			secretAccessKey
-		}
+			region,
+			credentials: {
+				accessKeyId,
+				secretAccessKey
+			}
 		});
-		const command = new RunInstancesCommand({
-			// some required parameters are missing here + the optional parameters
-			// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/ec2/command/RunInstancesCommand/
-			SubnetId: subnet.id,
-			ImageId: imageId
-		});
+		// validation of the options object can take place here
+		const command = new RunInstancesCommand(options);
 		const response = await ec2Client.send(command);
-		return {
-			metadata: response.$metadata,
-			groups: response.Groups, // has the description "Not supported." in the documentation, unsure if needed
-			instances: response.Instances,
-			ownerId: response.OwnerId,
-			requesterId: response.RequesterId,
-			reservationId: response.reservationId
-		};
+		return response;
 	}
 
 	async function createResource({
