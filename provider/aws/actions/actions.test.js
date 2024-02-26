d6a4fe4b0c38ecac378dc15181ed2e00869ea5a8
Rohn Allen Ramos Santiago <rsantiag@tcd.ie>
2024-02-26 19:54:53 +0000

    Added tests for subnet actions

--- provider/aws/actions/actions.test.js
+++ provider/aws/actions/actions.test.js
@@ -1,7 +1,8 @@
 import { mockClient } from "aws-sdk-client-mock";
-import { EC2Client, CreateVpcCommand, DescribeVpcsCommand } from "@aws-sdk/client-ec2";
+import { EC2Client, CreateVpcCommand, DescribeVpcsCommand,
+        CreateSubnetCommand, DescribeSubnetsCommand } from "@aws-sdk/client-ec2";
 import { createVpc, describeVpcs, deleteVPC } from "./vpc-actions.js";
-import { expectTypeOf } from 'expect-type';
+import { createSubnet, describeSubnets, deleteSubnet } from "./subnet-actions.js";
 
 const ec2Mock = mockClient(EC2Client);
 beforeEach(() => {
@@ -15,7 +16,8 @@ test("Determines if createVpc returns a vpc object", async () => {
     }).resolves({
         "Vpc": {
             "AmazonProvidedIpv6CidrBlock": true,
-            "CidrBlock": "172.0.0.0"
+            "CidrBlock": "172.0.0.0",
+            "VpcId": "vpc-abc123"
         }
     });
 
@@ -27,12 +29,13 @@ test("Determines if createVpc returns a vpc object", async () => {
     expect(vpc).toStrictEqual({
         "Vpc": {
             "AmazonProvidedIpv6CidrBlock": true,
-            "CidrBlock": "172.0.0.0"
+            "CidrBlock": "172.0.0.0",
+            "VpcId": "vpc-abc123"
         }
     });
 })
 
-test("Determines if describeVpcs returns a list of vpcs corresponding toinoutted vpcIds", async () => {
+test("Determines if describeVpcs returns a list of vpcs corresponding to inputted vpcIds", async () => {
     ec2Mock.on(CreateVpcCommand, {
         "AmazonProvidedIpv6CidrBlock": true,
         "CidrBlock": "172.0.0.0"
@@ -40,7 +43,7 @@ test("Determines if describeVpcs returns a list of vpcs corresponding toinoutted
         "Vpc": {
             "AmazonProvidedIpv6CidrBlock": true,
             "CidrBlock": "172.0.0.0",
-            "VpcId": "abc123"
+            "VpcId": "vpc-abc123"
         }
     });
 
@@ -51,26 +54,26 @@ test("Determines if describeVpcs returns a list of vpcs corresponding toinoutted
         "Vpc": {
             "AmazonProvidedIpv6CidrBlock": true,
             "CidrBlock": "174.0.0.0",
-            "VpcId": "def456"
+            "VpcId": "vpc-def456"
         }
     });
 
     ec2Mock.on(DescribeVpcsCommand, {
         "VpcIds": [
-            "abc123",
-            "def456"
+            "vpc-abc123",
+            "vpc-def456"
         ]
     }).resolves({
         "Vpcs": [
             {
                 "AmazonProvidedIpv6CidrBlock": true,
                 "CidrBlock": "172.0.0.0",
-                "VpcId": "abc123"
+                "VpcId": "vpc-abc123"
             },
             {
                 "AmazonProvidedIpv6CidrBlock": true,
                 "CidrBlock": "174.0.0.0",
-                "VpcId": "def456"
+                "VpcId": "vpc-def456"
             }
         ]
     });
@@ -83,25 +86,179 @@ test("Determines if describeVpcs returns a list of vpcs corresponding toinoutted
         "AmazonProvidedIpv6CidrBlock": true,
         "CidrBlock": "174.0.0.0"
     });
-    const vpcList = await describeVpcs(ec2Mock, ["abc123", "def456"]);
+    const vpcList = await describeVpcs(ec2Mock,
+        [vpc1.Vpc.VpcId, vpc2.Vpc.VpcId]);
 
     expect(vpcList).toStrictEqual({
         "Vpcs": [
             {
                 "AmazonProvidedIpv6CidrBlock": true,
                 "CidrBlock": "172.0.0.0",
-                "VpcId": "abc123"
+                "VpcId": "vpc-abc123"
             },
             {
                 "AmazonProvidedIpv6CidrBlock": true,
                 "CidrBlock": "174.0.0.0",
-                "VpcId": "def456"
+                "VpcId": "vpc-def456"
             }
         ]
     });
 })
 
 test("Determines if deleteVpc returns successfully", () => {
-    expect(() => deleteVPC(ec2Mock, "abc123")).not.toThrow();
+    expect(() => deleteVPC(ec2Mock, "vpc-abc123")).not.toThrow();
 })
+
+test("Determines if createSubnet returns a subnet object", async () => {
+    ec2Mock.on(CreateVpcCommand, {
+        "AmazonProvidedIpv6CidrBlock": true,
+        "CidrBlock": "172.0.0.0"
+    }).resolves({
+        "Vpc": {
+            "AmazonProvidedIpv6CidrBlock": true,
+            "CidrBlock": "172.0.0.0",
+            "VpcId": "vpc-abc123"
+        }
+    });
+
+    ec2Mock.on(CreateSubnetCommand, {
+        "CidrBlock": "172.0.0.0",
+        "VpcId": "vpc-abc123"
+    }).resolves({
+        "Subnet": {
+            "AvailabilityZone": "eu-north-1",
+            "CidrBlock": "172.0.0.0",
+            "SubnetId": "subnet-abc123",
+            "VpcId": "vpc-abc123"
+        }
+    })
+
+    const vpc = await createVpc(ec2Mock, {
+        "AmazonProvidedIpv6CidrBlock": true,
+        "CidrBlock": "172.0.0.0"
+    });
+    const subnet = await createSubnet(ec2Mock, {
+        "CidrBlock": "172.0.0.0",
+        "VpcId": vpc.Vpc.VpcId
+    })
+    expect(subnet).toStrictEqual({
+        "Subnet": {
+            "AvailabilityZone": "eu-north-1",
+            "CidrBlock": "172.0.0.0",
+            "SubnetId": "subnet-abc123",
+            "VpcId": "vpc-abc123"
+        }
+    })
+})
+
+test("Determines if describeSubnets returns a list of subnets corresponding to inputted subnetIds", async () => {
+    ec2Mock.on(CreateVpcCommand, {
+        "AmazonProvidedIpv6CidrBlock": true,
+        "CidrBlock": "172.0.0.0"
+    }).resolves({
+        "Vpc": {
+            "AmazonProvidedIpv6CidrBlock": true,
+            "CidrBlock": "172.0.0.0",
+            "VpcId": "vpc-abc123"
+        }
+    });
+
+    ec2Mock.on(CreateVpcCommand, {
+        "AmazonProvidedIpv6CidrBlock": true,
+        "CidrBlock": "174.0.0.0"
+    }).resolves({
+        "Vpc": {
+            "AmazonProvidedIpv6CidrBlock": true,
+            "CidrBlock": "174.0.0.0",
+            "VpcId": "vpc-def456"
+        }
+    });
+
+    ec2Mock.on(CreateSubnetCommand, {
+        "CidrBlock": "172.0.0.0",
+        "VpcId": "vpc-abc123"
+    }).resolves({
+        "Subnet": {
+            "AvailabilityZone": "eu-north-1",
+            "CidrBlock": "172.0.0.0",
+            "SubnetId": "subnet-abc123",
+            "VpcId": "vpc-abc123"
+        }
+    });
+
+    ec2Mock.on(CreateSubnetCommand, {
+        "CidrBlock": "174.0.0.0",
+        "VpcId": "vpc-def456"
+    }).resolves({
+        "Subnet": {
+            "AvailabilityZone": "eu-north-1",
+            "CidrBlock": "174.0.0.0",
+            "SubnetId": "subnet-def456",
+            "VpcId": "vpc-def456"
+        }
+    });
+
+    ec2Mock.on(DescribeSubnetsCommand, {
+        SubnetIds: [
+            "subnet-abc123",
+            "subnet-def456"
+        ]
+    }).resolves({
+        Subnets: [
+            {
+                "AvailabilityZone": "eu-north-1",
+                "CidrBlock": "172.0.0.0",
+                "SubnetId": "subnet-abc123",
+                "VpcId": "vpc-abc123"
+            },
+            {
+                "AvailabilityZone": "eu-north-1",
+                "CidrBlock": "174.0.0.0",
+                "SubnetId": "subnet-def456",
+                "VpcId": "vpc-def456"
+            }
+        ]
+    });
+
+    const vpc1 = await createVpc(ec2Mock, {
+        "AmazonProvidedIpv6CidrBlock": true,
+        "CidrBlock": "172.0.0.0"
+    });
+    const vpc2 = await createVpc(ec2Mock, {
+        "AmazonProvidedIpv6CidrBlock": true,
+        "CidrBlock": "174.0.0.0"
+    });
+    const subnet1 = await createSubnet(ec2Mock, {
+        "CidrBlock": "172.0.0.0",
+        "VpcId": vpc1.Vpc.VpcId
+    });
+    const subnet2 = await createSubnet(ec2Mock, {
+        "CidrBlock": "174.0.0.0",
+        "VpcId": vpc2.Vpc.VpcId
+    });
+    const subnetList = await describeSubnets(ec2Mock,
+        [subnet1.Subnet.SubnetId, subnet2.Subnet.SubnetId]);
+    expect(subnetList).toStrictEqual({
+        Subnets: [
+            {
+                "AvailabilityZone": "eu-north-1",
+                "CidrBlock": "172.0.0.0",
+                "SubnetId": "subnet-abc123",
+                "VpcId": "vpc-abc123"
+            },
+            {
+                "AvailabilityZone": "eu-north-1",
+                "CidrBlock": "174.0.0.0",
+                "SubnetId": "subnet-def456",
+                "VpcId": "vpc-def456"
+            }
+        ]
+    });
+})
+
+test("Determines if deleteSubnet returns successfully", () => {
+    expect(() => deleteSubnet(ec2Mock, "subnet-abc123")).not.toThrow();
+})
+
+
 
