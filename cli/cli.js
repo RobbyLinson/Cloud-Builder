7e0cc6354e327cd667f9e7de26de591af02b41a4
Harroche <119439246+Harroche@users.noreply.github.com>
2024-02-27 14:39:40 +0000

    Fixed async error in code

--- cli/cli.js
+++ cli/cli.js
@@ -6,7 +6,9 @@ import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 import chalk from 'chalk';
 import fs from 'fs';
-import * as credentials from '../cliCred/credentials';
+import os from 'os';
+import read from 'readline';
+
 //const boxen = require('boxen');
 
 
@@ -26,8 +28,9 @@ console.log("Welcome to cloud builder");
 
 console.log("\n commands:\n greet-Cli  greet yourNameHere       Gives you a little greeting!")
 console.log("greet-Cli run testFileNameHere                  Runs given file.")
-
-credentials.checkAndPopulate();
+const filePath = os.homedir();
+console.log(filePath + " and the opereating system is "+ os.type());
+checkAndPopulate();
 
 // Use yargs to define commands and their callbacks
 yargs(hideBin(process.argv))
@@ -65,5 +68,120 @@ yargs(hideBin(process.argv))
     })
     .parse();
   
+function readDir(filePath,callback){
+    fs.readdir(filePath,(err,file) =>{
+        if(err)
+        {
+            console.error("error occured",err)
+            callback([]);
+        }
+        callback(file);
+    });
+}
+
+async function createObjectForIni (){
+    const readLine = read.Interface({
+        input:process.stdin,
+        output:process.stdout
+    });
+    let questionArr = [];
+
+    function askQuestion(query)
+    {
+        return new Promise(resolve =>{
+            readLine.question(query,response=>{
+                questionArr.push(response);
+                resolve();
+            });
+        });
+    }
+    await askQuestion('What is your AWS key? ');
+    await askQuestion('What is your AWS secret key? ');
+    readLine.close();
+    return questionArr;
+}
+
+
+async function wrtieFileAsync(path,data){
+    return new Promise((resolve,reject)=>{
+        fs.writeFile(path,data,(err)=>{
+            if(err){
+                reject(err);
+            } else{
+                resolve();
+            }
+        });
+    });
+}
+
+async function main(){
+    try{
+        const arr= await createObjectForIni();
+        const awsFilePath =filePath+(os.type()==='Windows_NT' ? "\\.aws\\" : "/.aws/"); //what os type
+        let duplicateArr = ["[default]","aws_access_key_id=","aws_secret_access_key="];
+        for(let i =0; i<arr.length; i++)
+        {
+            duplicateArr[i+1]=duplicateArr[i+1] + arr[i];
+        }
+        const outputString = duplicateArr.join("\n");
+        await wrtieFileAsync(awsFilePath+"credentials.ini",outputString);
+        const stringToConfig="[default]\nregion=eu-west-1\noutput=json";
+        await wrtieFileAsync(awsFilePath+"config.ini",stringToConfig);
+    } catch {
+        console.error("problem");
+    }
+}
+
+function createAwsFoulder(){
+    const creationOfFoldier = filePath+(os.type()==='Windows_NT' ? "\\.aws" : "/.aws");
+    fs.mkdir(creationOfFoldier,(err)=>{
+        if(err)
+        {
+            return console.error(err);
+        }
+    });
+}
+
+async function checkAndPopulate()
+{ 
+    readDir(filePath,function(files){
+        let exists = false;
+        for(const val of files)
+        {
+            if(val == '.aws')
+            {
+                exists = true;
+            }
+        }
+        if(!exists)
+        {
+            createAwsFoulder();
+            main();
+        }
+        else
+        {
+            const awsFouilderPath = filePath+(os.type()==='Windows_NT' ? "\\.aws" : "/.aws");
+            readDir(awsFouilderPath,function(filesInAWS){
+                let config = false;
+                let cred = false;
+                for(const val of filesInAWS)
+                {
+                    if(val == 'credentials.ini')
+                    {
+                        cred = true;
+                    }
+                    else if (val == 'config.ini')
+                    {
+                        config=true;
+                    }
+                }
+                if(!cred && !config)
+                {
+                    main();
+                }
+            });
+        }
+    });
+}
 
 
