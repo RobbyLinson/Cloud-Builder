f0bcf32127c36bd46444788b7c915dda52e77182
Robert Mee Lucey <robertmeelucey@gmail.com>
2024-04-03 14:24:06 +0100

    added route tables methods, renamed some route table provider methods

--- provider/aws/actions/routetable-actions.js
+++ provider/aws/actions/routetable-actions.js
@@ -1,4 +1,4 @@
-import { CreateRouteTableCommand, DescribeRouteTablesCommand, DeleteRouteTableCommand, AssociateRouteTableCommand} from "@aws-sdk/client-ec2";
+import { CreateRouteTableCommand, DescribeRouteTablesCommand, DeleteRouteTableCommand, AssociateRouteTableCommand, CreateRouteCommand, DeleteRouteCommand, DisassociateRouteTableCommand} from "@aws-sdk/client-ec2";
 
 export async function createRouteTable(ec2Client, {
     name,
@@ -38,9 +38,9 @@ export async function deleteRouteTable(ec2Client,routetableId) {
     } catch (err) {
       console.warn(`Failed to terminate instance ${routetableId}.`, err);
     }
-};
+}
 
-export async function attachRouteTable(ec2Client, routetableId, subnetId ) {
+export async function attachRouteTableToSubnet(ec2Client, routetableId, subnetId ) {
 	const command = new AssociateRouteTableCommand({RouteTableId: routetableId,
 	SubnetId: subnetId
 	});
@@ -50,4 +50,67 @@ export async function attachRouteTable(ec2Client, routetableId, subnetId ) {
 	} catch (err) {
 		console.warn(`Failed to attach subnet to routetable.`, err);
 	}
+}
+
+export async function detachRouteTableFromSubnet(ec2Client, routetableId, subnetId ) {
+
+	const assocId = await getAssociationId(ec2Client, routetableId, subnetId);
+	const command = new DisassociateRouteTableCommand({
+		AssociationId: assocId
+	});
+	try {
+		await ec2Client.send(command);
+		console.log(`✂️ Detached routetable ${routetableId} from subnet ${subnetId}\n`);
+	} catch (err) {
+		console.warn(`Failed to detach subnet from routetable.`, err);
+	}
+}
+
+export async function attachRouteTableToGateway(ec2Client, destinationcidrblock, gatewayId, routetableId ) {
+	const command = new CreateRouteCommand({DestinationCidrBlock: destinationcidrblock,
+	GatewayId: gatewayId,
+	RouteTableId: routetableId
+	});
+	try{
+		const response = await ec2Client.send(command);
+		console.log(`🤝 Attached gateway ${gatewayId} to routetable ${routetableId}\n`);
+	} catch (err) {
+		console.warn(`Failed to attach gateway to routetable.`, err);
+	}
+}
+
+export async function detachRouteTableFromGateway(ec2Client, destinationcidrblock, routetableId ) {
+	const command = new DeleteRouteCommand({DestinationCidrBlock: destinationcidrblock,
+	RouteTableId: routetableId
+	});
+	try{
+		const response = await ec2Client.send(command);
+		console.log(`✂️ Detached internet gateway from routetable ${routetableId}\n`);
+	} catch (err) {
+		console.warn(`Failed to attach gateway to routetable.`, err);
+	}
+}
+
+export async function getAssociationId(ec2Client, routeTableId, subnetId) {
+    try {
+        const data = await ec2Client.send(new DescribeRouteTablesCommand({
+            RouteTableIds: [routeTableId]
+        }));
+
+        const routeTable = data.RouteTables.find(table => table.RouteTableId === routeTableId);
+        if (!routeTable) {
+            console.error('Route table not found');
+            return;
+        }
+
+        const association = routeTable.Associations.find(assoc => assoc.SubnetId === subnetId);
+        if (!association) {
+            console.error('Association not found');
+            return;
+        }
+
+        return association.RouteTableAssociationId;
+    } catch (err) {
+        console.error(err, err.stack);
+    }
 }
